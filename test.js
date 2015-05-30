0a53c1bc3be73ef588b74ca33524c50028837d21
alfred sang <i5ting@126.com>
2015-05-30 08:57:20 +0800

    out json

--- test.js
+++ test.js
@@ -1,25 +1,29 @@
 var dirWalker = require('./lib/dirWalker');
 var fs = require('fs');
 
+var output = {
+  origin:[]
+};
+
+
 function handleFile(path, floor) {
 	var blankStr = '';
 	for (var i = 0; i < floor; i++) {
 		blankStr += '    ';
 	}
-  
+  // console.log(floor)
+  if(floor ==1){
+    return;
+  }
   if(!path.match(/node_modules$/)){
     return;
   }
   
-  // remove .bin $
-  if(path.match(/bin$/i)){
-    console.log('xxx' + path)
-    return;
-  }
-  
+  // console.log(path)
 	fs.stat(path, function(err1, stats) {
 		if (err1) {
-			console.log('stat error');
+      console.dir(err1);
+      // console.log('stat error');
 		} else {
 			if (stats.isDirectory()) {
         // console.log('+' + blankStr + path);
@@ -27,6 +31,40 @@ function handleFile(path, floor) {
         
         dirWalker.dir(path,function(p){
           console.log(p);
+          
+          var fs = require('fs')
+
+          var name = p.split('/').pop()
+          console.log(name);
+          
+          if(name === '.bin'){
+            return;
+          }
+          // fs.link(srcpath, dstpath, callback)#
+          var srcpath = p;
+          var dstpath = "./test_node_modules/" + name;
+          
+          var packagejson = p + "/package.json"
+          
+          var package_version = require(packagejson).version;
+          var package_description = require(packagejson).description;
+          console.log('version = ' + package_version);
+          
+          output.origin.push({
+            name    :name,
+            version :package_version,
+            path    :p,
+            desc    :package_description
+          })
+          
+          var c = JSON.stringify(output, null, 4);
+          fs.writeFileSync('./out.json', c);
+          //
+          // fs.symlink(srcpath, dstpath, function (err, resolvedPath) {
+          //   if (err) throw err;
+          //   console.log(resolvedPath);
+          // });
+          
         })
         
 			} else {
@@ -37,3 +75,5 @@ function handleFile(path, floor) {
 }
 
 dirWalker.walk('.', 0, handleFile);
+
+
